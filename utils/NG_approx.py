544018da9b4b3bd4444b6908dbc963877fb0a8f9
chenxy <chxy517@bu.edu>
2023-08-15 21:30:21 -0400

    update

--- utils/NG_approx.py
+++ utils/NG_approx.py
@@ -1,14 +1,31 @@
+import math
+import scipy.special
+import scipy.constants
+import numpy as np
 
-ana = []
+def lq(x, q):
+    return np.log(x) / np.log(1/q)
+
+def four(k, q):
+    Q = 1/q
+    logQ = np.log(Q)
+    numerator = -1 / (logQ)
+    argument = -2 * k * np.pi * 1j / logQ
+    gamma_term = scipy.special.loggamma(argument)
+    result = numerator * gamma_term
+    return result
+    
 p = 0.05
-q = 1 - p
+q = 1-p
+N = 2
 
-k = 5
-for n in range(1, 20):
-    u = 1
-    for x in range(0, k):
-        u *= (1 - ((1 - q ** x) ** n))
-    u += n * (q ** k) / p
-    ana.append(u)
+approx = []
 
-print(ana*2)
+for b in range(1, 11):
+    x = lq(N, q) + (b-1)*lq(lq(N, q), q) + (b-1)*lq(p,q) + (b-1) - lq(math.factorial(b - 1), q)
+    E_nb = x + 0.5 + (0.57721566490153286060651209)/lq(1/q, q)
+    approx.append(E_nb)
+
+print(approx)
+
+print(lq(lq(N,q),q))
